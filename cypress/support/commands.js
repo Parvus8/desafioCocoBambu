71ab77a1896321e308764933898ef33735753cf3
JazzAuto <pigmeu.reddit2@gmail.com>
2025-03-12 23:05:30 -0300

    adicao de comandos no arquivo determinado

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -6,3 +6,18 @@ Cypress.Commands.add('searchAddress', (address) => {
   Cypress.Commands.add('selectFirstSuggestion', () => {
     cy.get('[data-cy="address-suggestion"]').first().click();
   });
+  
+Cypress.Commands.add('screenshotWithTimestamp', (name) => {
+    const timestamp = new Date().toLocaleString();
+    const fileName = `${name}-${timestamp}`;
+
+    cy.log(`Taking screenshot: ${fileName}`);
+
+    cy.screenshot(fileName, { capture: 'viewport' });
+
+    cy.get('body').then(($body) => {
+        const $dateTime = Cypress.$(`<div>${timestamp}</div>`);
+        $body.append($dateTime);
+        cy.log(`Timestamp: ${timestamp}`);
+    });
+});
